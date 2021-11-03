af58b95bceaec23af0e1042ea570b6d31fd59b3d
GLORIA VILLASENOR <82358971+GloriaVillasenor@users.noreply.github.com>
2021-11-02 19:24:22 -0700

    Added todo express to week 4

--- week4/todo/routes/todoRouter.js
+++ week4/todo/routes/todoRouter.js
@@ -24,13 +24,16 @@ todoRouter.route('/')
 
 todoRouter.route('/:id')
     .delete((req, res) => {
-        const todosId = req.params.todosId
+        const todosId = req.params.id
+        const index = todos.findIndex(todo => todo._id === todosId)
+        todos.splice(index, 1)
         res.send("successfully deleted!")
     })
     .put((req, res) => {
         const todo = req.body
-        todo._id = uuidv4()
-        todos.push(todo)
+        const todosId = req.params.id
+        const index = todos.findIndex(todo => todo._id === todosId)
+        const updatedtodo = Object.assign(todos[index], todo)
         res.send(`Added new(todo: $(todo.name} $(todo.description}`)
     })
 
